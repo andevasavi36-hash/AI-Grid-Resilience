15d8c9d4d7957850dbd02616bcb04a91cc2fe561
Ande Vasavi <andevasavi36@gmail.com>
2026-08-10 15:21:19 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 from fastapi import FastAPI
-from database.schema import GridInput
-from database.database import supabase
+from fastapi.middleware.cors import CORSMiddleware
+
+from schema import GridInput
 from predict import predict_risk
 
-app = FastAPI()
+from supabase import create_client
+from config import SUPABASE_URL, SUPABASE_KEY
+
+
+# =========================
+# FastAPI App
+# =========================
+
+app = FastAPI(
+    title="AI Grid Resilience Prediction API",
+    description="Predictive Fault Risk Analysis for Power Grid Assets",
+    version="1.0"
+)
+
+
+# =========================
+# CORS
+# =========================
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# =========================
+# Supabase Connection
+# =========================
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
+
+
+# =========================
+# Home
+# =========================
 
 @app.get("/")
 def home():
@@ -11,6 +52,10 @@ def home():
         "message": "AI Grid Resilience Prediction API is Running"
     }
 
+
+# =========================
+# Prediction
+# =========================
 
 @app.post("/predict")
 def prediction(data: GridInput):
@@ -21,12 +66,16 @@ def prediction(data: GridInput):
     # Model Prediction
     result = predict_risk(input_data)
 
-    # Save into Supabase
+    # Add prediction result
     input_data["Risk_Percentage"] = result
 
-    supabase.table("grid_resilience_dataset").insert(
-        input_data
-    ).execute()
+    # Save prediction to Supabase
+    try:
+        supabase.table("grid_resilience_dataset").insert(
+            input_data
+        ).execute()
+    except Exception as e:
+        print("Supabase insert error:", e)
 
     # Return Prediction
     return {
